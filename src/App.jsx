ab20b3cb0481477fe96a5803040bd16c17bf4dc9
astratow <davno_pora@mail.com>
2024-09-09 23:59:32 +0100

    List and Person add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import {useState } from 'react';
 import data from "./data";
+import List from './list';
+import Person from './Person';
 
 const App = () => {
 	const [people, setPeople] = useState(data);
 	console.log(people);
-  return <h2>Birthday Reminder - Starter</h2>;
+  return <main>
+		<section className='container'>
+			<h3>{people.length} birthdays today</h3>
+			<List people={people} />
+		</section>
+	  {/* <h2>Birthday Reminder - Starter</h2>; */}
+  </main>
 };
 export default App;
---
+++ src/Person.jsx
@@ -0,0 +1,6 @@
+const Person = ({ image, name, age}) => {
+    return <article className="person">
+        <img src="{image}" alt="{name}" className="{img}" />
+    </article>
+};
+export default Person;
