9ee5329ead09e9b1746699006be2c5ff8c665474
astratow <davno_pora@mail.com>
2024-09-10 20:47:17 +0100

    button

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ const App = () => {
 		<section className='container'>
 			<h3>{people.length} birthdays today</h3>
 			<List people={people} />
+			<button 
+				type='button'
+				className='btn btn-block'
+				onClick={() => setPeople([])}
+			>clear all</button>
 		</section>
 	  {/* <h2>Birthday Reminder - Starter</h2>; */}
   </main>
